389f093fc4ac6fb56358f075261a55d3f4227ba4
= <=>
2021-05-01 02:10:29 -0300

    menu hamburguesa

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import hamburgerMenu from "./DOMEvents/hamburger_menu.js";
+
+document.addEventListener("DOMContentLoaded", e =>{
+
+    hamburgerMenu(".panel-btn",".main-header__ul",".main-header__ul a");
+
+})
