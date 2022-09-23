f18fb946dd38685d396dfa46726600c34db438f9
Alan <tapia.alan.g@gmail.com>
2022-09-23 10:31:31 -0300

    fixes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ import hamburgerMenu from "./DOMEvents/hamburger_menu.js";
 
 document.addEventListener("DOMContentLoaded", e =>{
 
-    hamburgerMenu(".panel-btn",".main-header__ul",".main-header__ul a");
+    hamburgerMenu(".panel-btn",".nav-menu",".nav-link");
+
+
+    // Form
+    const formulario = document.querySelector('form')
+
+    formulario.addEventListener("submit", e => {
+        e.preventDefault()
+        console.log(e)
+
+        alert('Datos enviados!')
+        formulario.reset()
+      });
 
 })
